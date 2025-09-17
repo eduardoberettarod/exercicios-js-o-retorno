8232b8edd98603a8bace23be26e0bb5495147fa9
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-09-16 22:22:01 -0300

    FINALIZAÇÃO
    TERMINEI DE FAZER O EXERCICIO 4 DE JAVASCRIPT

---
+++ exercicios004/script.js
@@ -0,0 +1,17 @@
+function fnCalcular(){
+    const nome = document.getElementById("nome").value;
+    const idade = parseInt(document.getElementById("idade").value);
+    const putao = document.getElementById("putao");
+    const feliz = document.getElementById("feliz");
+    const resultado = document.getElementById("resultado");
+
+    eventpreventDefault();
+    if(idade >= 18){
+        resultado.innerText = "PARABÉNS " + nome + ", VOCÊ TEM " + idade + " E PODE TIRAR A CNH!";
+    }
+    else{
+        resultado.innerText = "Sinto muito " + nome + ", infelizmente você não tem a idade suficiente para tirar a CNH! BETINHA NASCE MAIS CEDO";
+        putao.style.display = "block";
+        feliz.style.display = "none";
+    }
+}
