f71c1dfaec2bb99161ce4314af97a9c6724f618d
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-09-23 21:14:59 -0300

    arrumando o exercicio 7

--- exercicio007/script.js
+++ exercicio007/script.js
@@ -1,18 +1,42 @@
 let dataAtual = new Date()
 let dia = dataAtual.getDay()
 
-if(dia == 0){
-    document.getElementById('resposta').innerText = "Domingo"
-}else if(dia == 1){
-    document.getElementById('resposta').innerText = "Segunda-Feira"
-}else if (dia == 2){
-    document.getElementById('resposta').innerText = "Terça-Feira"
-}else if(dia == 3){
-    document.getElementById('resposta').innerText = "Quarta-Feira"
-}else if(dia == 4){
-    document.getElementById('resposta').innerText = "Quinta-Feira"
-}else if (dia == 5){
-    document.getElementById('resposta').innerText = "Sexta-Feira"
-}else if(dia == 6){
-    document.getElementById('resposta').innerText = "Sábado"
+// if(dia == 0){
+//     document.getElementById('resposta').innerText = "Domingo"
+// }else if(dia == 1){
+//     document.getElementById('resposta').innerText = "Segunda-Feira"
+// }else if (dia == 2){
+//     document.getElementById('resposta').innerText = "Terça-Feira"
+// }else if(dia == 3){
+//     document.getElementById('resposta').innerText = "Quarta-Feira"
+// }else if(dia == 4){
+//     document.getElementById('resposta').innerText = "Quinta-Feira"
+// }else if (dia == 5){
+//     document.getElementById('resposta').innerText = "Sexta-Feira"
+// }else if(dia == 6){
+//     document.getElementById('resposta').innerText = "Sábado"
+// }
+
+switch (dia){
+    case 0:
+        document.getElementById("resposta").innerText = "Domingo"
+        break
+    case 1:
+        document.getElementById("resposta").innerText = "Segunda-Feira"
+        break
+    case 2:
+        document.getElementById("resposta").innerText = "Terça-Feira"
+        break
+    case 3:
+        document.getElementById("resposta").innerText = "Quarta-Feira"
+        break
+    case 4:
+        document.getElementById("resposta").innerText = "Quinta-Feira"
+        break
+    case 5:
+        document.getElementById("resposta").innerText = "Sexta-Feira"
+        break
+    case 6:
+        document.getElementById("resposta").innerText = "Sábado"
+        break
 }
