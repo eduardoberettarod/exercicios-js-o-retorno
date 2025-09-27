2936b75fbec195429455d17c4888deba6c4eb22b
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-09-26 22:25:48 -0300

    exercicio 10
    finalizacao do exercicio 10

---
+++ bonus001/script.js
@@ -0,0 +1,12 @@
+function fnTocarSom(tecla) {
+    if (tecla == 65) {
+        document.getElementById("som-65").currentTime = 0
+        document.getElementById("som-65").play()
+    } else if (tecla == 83) {
+        document.getElementById("som-83").currentTime = 0
+        document.getElementById("som-83").play()
+    } else if (tecla == 68) {
+        document.getElementById("som-68").currentTime = 0
+        document.getElementById("som-68").play()
+    }else if (tecla ==)
+}
