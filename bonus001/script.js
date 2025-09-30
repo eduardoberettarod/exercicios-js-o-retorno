7b26ea1bcad620ef4757a6375e65cbcda854df4a
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-09-30 20:39:42 -0300

    exercicio 11
    resolvendo o exercicio 11 de javascript

--- bonus001/script.js
+++ bonus001/script.js
@@ -1,30 +1,29 @@
+let teclasPressionadas = {}
+
 function fnTocarSom(tecla) {
-    if (tecla == 65) {
+    teclasPressionadas[tecla] = true
+
+    // Exemplo: E (69) + L (76) abre modal
+    if (teclasPressionadas[69] && teclasPressionadas[76]) {
+        abrirModal()
+    }
+
+    // Teclas individuais continuam funcionando (exemplo)
+    if (tecla == 65) { // A
         document.getElementById("som-65").currentTime = 0
         document.getElementById("som-65").play()
-    } else if (tecla == 83) {
-        document.getElementById("som-83").currentTime = 0
-        document.getElementById("som-83").play()
-    } else if (tecla == 68) {
-        document.getElementById("som-68").currentTime = 0
-        document.getElementById("som-68").play()
-    }else if (tecla == 70) {
-        document.getElementById("som-70").currentTime = 0
-        document.getElementById("som-70").play()
-    }else if (tecla == 71) {
-        document.getElementById("som-71").currentTime = 0
-        document.getElementById("som-71").play()
-    }else if (tecla == 72) {
-        document.getElementById("som-72").currentTime = 0
-        document.getElementById("som-72").play()
-    }else if (tecla == 74) {
-        document.getElementById("som-74").currentTime = 0
-        document.getElementById("som-74").play()
-    }else if (tecla == 75) {
-        document.getElementById("som-75").currentTime = 0
-        document.getElementById("som-75").play()
-    }else if (tecla == 76) {
-        document.getElementById("som-76").currentTime = 0
-        document.getElementById("som-76").play()
     }
+}
+
+document.addEventListener("keyup", (event) => {
+    delete teclasPressionadas[event.keyCode]
+})
+
+// Funções do modal
+function abrirModal() {
+    document.getElementById("meuModal").style.display = "flex"
+}
+
+function fecharModal() {
+    document.getElementById("meuModal").style.display = "none"
 }
