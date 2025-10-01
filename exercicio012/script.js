326bb85262034e3c92c4e55f8cb8665e89e87668
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-09-30 21:54:21 -0300

    exercicio 12
    resolvendo o exercicio 12 de javascript

---
+++ exercicio012/script.js
@@ -0,0 +1,28 @@
+//chama a função de acordo com um tempo preestabelecido
+
+// setInterval(function(){
+//     alert("teste")
+// }, 5000)
+
+let contador = 0, controle = 0
+
+function fnStart() {
+    document.getElementById("contador").innerHTML = contador
+
+    if (controle == 0) {
+        contador++
+    }else if(controle == 10){
+        contador--
+    }
+    if(contador == 10){
+        controle = 10
+    }else if(contador == 0){
+        controle = 0
+    }
+}
+
+function fnLigar() {
+    setInterval(fnStart, 1000)
+    document.getElementById("botao").disabled = true
+}
+    
